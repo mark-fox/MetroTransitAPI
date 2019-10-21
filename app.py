e137c002affabdd956934f68a878041d23c7e5dc
secretuser <secretuser@email.com>
2019-10-20 23:43:52 -0500

    Added start of requests process to get initial data

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests, json
 
 app = Flask(__name__)
+URL = 'http://svc.metrotransit.org/nextrip/{}?format=json'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
-    return render_template('index.html')
+    res = requests.get(URL.format('routes'))
+    resData = res.json()
+
+    if request.method == 'GET':
+        return render_template('index.html', routes = resData)
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
     # TODO remove debug=True text when finalizing
     app.run(debug=True)
+# TODO pip freeze > requirements.txt
