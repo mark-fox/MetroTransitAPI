15c2c40e8f5aef7d99aad6eb68a5388e0fc7e46c
secretuser <secretuser@email.com>
2019-10-23 19:07:07 -0500

    Comments

--- app.py
+++ app.py
@@ -1,41 +1,59 @@
-from flask import Flask, render_template, request, url_for, jsonify
-import requests, json
+from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__)
+# Designates location of front-end files
 app.static_folder = 'static'
+# API address plus formatting placeholder
 URL = 'http://svc.metrotransit.org/nextrip/{}?format=json'
 
 
+# Main page's route
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
+    # Generates Response from API for all Routes
     res = requests.get(URL.format('routes'))
+    # Converts Response to be sent to web page
     resData = res.json()
     return render_template('index.html', routes=resData)
 
 
+# Directions route
 @app.route('/directions', methods=['GET', 'POST'])
 def getDirections():
+    # Retrieves data from passed object
     routeNumber = request.args['routeNum']
+    # Generates Response from API using RouteID
     res = requests.get(URL.format('directions/' + routeNumber))
+    # Converts Response to be sent to AJAX callback
     return jsonify(res.json())
 
 
+# Stops route
 @app.route('/stops')
 def getStops():
+    # Retrieves data from passed object
     routeNumber = request.args['routeNum']
     direction = request.args['direction']
+    # Generates Response from API using RouteID and DirectionID
     res = requests.get(URL.format('stops/' + routeNumber + '/' + direction))
+    # Converts Response to be sent to AJAX callback
     return jsonify(res.json())
 
 
+# Departure Time route
 @app.route('/time')
 def getTime():
+    # Retrieves data from passed object
     routeNumber = request.args['routeNum']
     direction = request.args['direction']
     stop = request.args['stopNum']
+    # Generates Response from API using RouteID, DirectionID, and StopID
     res = requests.get(URL.format(routeNumber + '/' + direction + '/' + stop))
+    # Converts Response to be sent to AJAX callback
     return jsonify(res.json())
 
 
+# Run app
 if __name__ == '__main__':
     app.run()
--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,43 @@
+// Global variable holding generic placeholder text
 var timePlaceholderText = "(Select a location above)";
 
+// Main function ran at start
 $(function(){
+    // Adds Change event to the Routes list
     $('#routesId').change(function(){
+        // Replaces final result text area with placeholder text
         $("#timeId").text(timePlaceholderText);
+        // Clears Bus Stops list (for additional queries)
         $('#stopsId').empty();
+        // Adds title/instruction element to top of list
         addPlaceholder($('#stopsId'), 'Select Bus Stop');
+        // Retrieves Value from selected element
         var route = $("#routesId option:selected").val();
+        // Passes Value to function for Response
         requestDirections(route);
     })
+    // Adds Change event to the Directions list
     $('#directionId').change(function(){
+        // Replaces final result text area with placeholder text
         $("#timeId").text(timePlaceholderText);
+        // Retrieves Values from both selected elements
         var route = $("#routesId option:selected").val();
         var direction = $("#directionId option:selected").val();
+        // Passes Values to function for Response
         requestStops(route, direction);
     })
+    // Adds Change event to the Bus Stops list
     $('#stopsId').change(function(){
+        // Retrieves Values from all selected elements
         var route = $("#routesId option:selected").val();
         var direction = $("#directionId option:selected").val();
         var stop = $("#stopsId option:selected").val();
+        // Passes Values to function for Response
         requestTime(route, direction, stop);
     })
 });
 
-
+// Function for submitting AJAX request for Directions
 function requestDirections(route){
     $.ajax({
         method: "GET",
@@ -31,8 +46,11 @@ function requestDirections(route){
         contentType: 'application/json;charset=UTF-8',
         dataType: 'json',
         success: function(directions){
+            // Clears list from any previous queries
             $('#directionId').empty();
+            // Adds title/instruction element to top of list
             addPlaceholder($('#directionId'), 'Select Direction');
+            // Loops through Response and creates new Option element for each result
             for (var i = 0; i < directions.length; i++) {
                 $('#directionId').append('<option value="' + directions[i].Value + '">' + directions[i].Text + '</option>');
             }
@@ -40,7 +58,7 @@ function requestDirections(route){
     });
 };
 
-
+// Function for submitting AJAX request for Bus Stops
 function requestStops(route, direction){
     $.ajax({
         method: "GET",
@@ -49,8 +67,11 @@ function requestStops(route, direction){
         contentType: 'application/json;charset=UTF-8',
         dataType: 'json',
         success: function(stops){
+            // Clears list from any previous queries
             $('#stopsId').empty();
+            // Adds title/instruction element to top of list
             addPlaceholder($('#stopsId'), 'Select Bus Stop');
+            // Loops through Response and creates new Option element for each result
             for (var i = 0; i < stops.length; i++){
                 $('#stopsId').append('<option value="' + stops[i].Value + '">' + stops[i].Text + '</option>');
             }
@@ -58,7 +79,7 @@ function requestStops(route, direction){
     })
 };
 
-
+// Function for submitting AJAX request for Departure Time
 function requestTime(route, direction, stop){
     $.ajax({
         method:"GET",
@@ -67,32 +88,45 @@ function requestTime(route, direction, stop){
         contentType: 'application/json;charset=UTF-8',
         dataType: 'json',
         success: function(timeObj){
+            // Clears element from any previous results
             $('#timeId').empty();
             var timeString = "";
+            // Exception handler used to catch when API returns an empty object
             try {
                 // TODO figure out more elegant strategy
+                // Retrieves first item's DepartureTime value, removes extra characters,
+                // and converts value to integer
                 var arrivalTime = parseInt(timeObj[0].DepartureTime.slice(6, -7));
+                // Determines current time
                 var timeLeft = arrivalTime - new Date().getTime();
+                // Converts value to integer and minutes
                 timeLeft = parseInt(timeLeft/1000/60);
 
+                // Checks if next bus is less than an hour away
                 if (timeLeft < 60) {
+                    // Produces display string with result
                     timeString = timeLeft + " minutes";
+                // Checks if next bus is over an hour away
                 } else if (timeLeft > 60) {
+                    // Calculates hours and minutes from value
                     var hours = parseInt(timeLeft/60);
                     var mins = timeLeft%60;
+                    // Produces display string with result
                     timeString = hours + " hours " + mins + " minutes";
                 } else {
                     timeString = "Last bus has already departed";
                 }
             } catch(e){
+                // Produces display string with error message
                 timeString = "No Arrival times were found";
             }
+            // Adds display string to result element
             $('#timeId').text(timeString);
         }
     })
 };
 
-
+// Function for add title/instruction element to passed list
 function addPlaceholder(element, message){
     element.append('<option label="' + message + '" disabled class="optPlaceholder"></option>');
 }
